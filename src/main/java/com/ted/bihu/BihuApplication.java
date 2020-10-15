f2128c35b8045b16e7db5f4634fc70e60223ddd5
Ted <northcolor@foxmail.com>
2020-10-15 11:03:01 +0800

    init

---
+++ src/main/java/com/ted/bihu/BihuApplication.java
@@ -0,0 +1,13 @@
+package com.ted.bihu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BihuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BihuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ted/bihu/BihuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ted.bihu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BihuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
