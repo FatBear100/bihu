8a887303bef576b47318ae48dc4800ae987a453c
Ted <northcolor@foxmail.com>
2020-10-19 10:51:29 +0800

    use application.properties to configure

--- src/main/java/com/ted/bihu/controller/AuthController.java
+++ src/main/java/com/ted/bihu/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.ted.bihu.dto.AccessTokenDTO;
 import com.ted.bihu.dto.GitUser;
 import com.ted.bihu.provider.GitProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +15,24 @@ public class AuthController {
     @Autowired
     private GitProvider gitProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.client.uri}")
+    private String clientUri;
+
     @GetMapping("/callBack")
     public String callBack(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setState(state);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:9980/callBack");
-        accessTokenDTO.setClient_id("c83fa181e0b933b547bf");
-        accessTokenDTO.setClient_secret("64c8f5f1c9b17396a212b63189a97dc773d92dba");
+        accessTokenDTO.setRedirect_uri(clientUri);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         String token = gitProvider.getAccessToken(accessTokenDTO);
         GitUser gitUser = gitProvider.getUser(token);
         System.out.println(gitUser.getName());
