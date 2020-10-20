58d7a8c80db6018290263f016450c5511730c514
Ted <northcolor@foxmail.com>
2020-10-20 11:23:28 +0800

    增加h2数据库，存储token和用户信息

--- src/main/java/com/ted/bihu/controller/AuthController.java
+++ src/main/java/com/ted/bihu/controller/AuthController.java
@@ -2,12 +2,17 @@ package com.ted.bihu.controller;
 
 import com.ted.bihu.dto.AccessTokenDTO;
 import com.ted.bihu.dto.GitUser;
+import com.ted.bihu.mapper.UserMapper;
+import com.ted.bihu.model.User;
 import com.ted.bihu.provider.GitProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthController {
@@ -24,9 +29,14 @@ public class AuthController {
     @Value("${github.client.uri}")
     private String clientUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
+
     @GetMapping("/callBack")
     public String callBack(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setState(state);
         accessTokenDTO.setCode(code);
@@ -36,6 +46,19 @@ public class AuthController {
         String token = gitProvider.getAccessToken(accessTokenDTO);
         GitUser gitUser = gitProvider.getUser(token);
         System.out.println(gitUser.getName());
-        return "index";
+        if (gitUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(gitUser.getName());
+            user.setAccountId(String.valueOf(gitUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(System.currentTimeMillis());
+            userMapper.insert(user);
+
+            request.getSession().setAttribute("user", gitUser);
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
     }
 }
