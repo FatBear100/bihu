971734e66fdd4cc30bf9088d0f457736248d475a
Ted <northcolor@foxmail.com>
2020-11-02 16:38:48 +0800

    拦截器

--- src/main/java/com/ted/bihu/controller/IndexController.java
+++ src/main/java/com/ted/bihu/controller/IndexController.java
@@ -1,28 +1,17 @@
 package com.ted.bihu.controller;
 
 import com.ted.bihu.dto.PageDTO;
-import com.ted.bihu.dto.QuestionDTO;
-import com.ted.bihu.mapper.QuestionMapper;
-import com.ted.bihu.mapper.UserMapper;
-import com.ted.bihu.model.Question;
-import com.ted.bihu.model.User;
 import com.ted.bihu.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
-
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private QuestionService questionService;
@@ -33,20 +22,6 @@ public class IndexController {
                         @RequestParam(name = "pageIndex", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "3") Integer size
     ) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
-
         PageDTO pageDTO = questionService.list(page, size);
         model.addAttribute("page", pageDTO);
         return "index";
--- src/main/java/com/ted/bihu/controller/ProfileController.java
+++ src/main/java/com/ted/bihu/controller/ProfileController.java
@@ -1,7 +1,6 @@
 package com.ted.bihu.controller;
 
 import com.ted.bihu.dto.PageDTO;
-import com.ted.bihu.mapper.UserMapper;
 import com.ted.bihu.model.User;
 import com.ted.bihu.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
 
-    @Autowired
-    private UserMapper userMapper;
     @Autowired
     private QuestionService questionService;
 
@@ -29,19 +25,9 @@ public class ProfileController {
             @PathVariable(name = "action") String action,
             HttpServletRequest request,
             Model model){
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
+        User user = (User)request.getSession().getAttribute("user");
+        if (user == null){
+            return "redirect:/";
         }
         if (action.equals("questions")){
             model.addAttribute("section", "questions");
@@ -50,9 +36,7 @@ public class ProfileController {
             model.addAttribute("section", "replies");
             model.addAttribute("sectionName", "我的回复");
         }
-        if (user == null){
-            return "redirect:/";
-        }
+
         PageDTO pageDTO = questionService.list(user.getId(), page, size);
         model.addAttribute("page", pageDTO);
         return "profile";
